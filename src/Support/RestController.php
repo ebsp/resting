d6dcca0084e8f96ae1dff8b65bf2a7bae81b1510
Emil Petersen <emil@ebsp.dk>
2019-06-21 16:34:06 +0200

    Now possible to return a resourcable

--- src/Support/RestController.php
+++ src/Support/RestController.php
@@ -2,6 +2,8 @@
 
 namespace Seier\Resting\Support;
 
+use Exception;
+use Seier\Resting\Resource;
 use Illuminate\Http\Request;
 use Illuminate\Support\Collection;
 
@@ -18,6 +20,8 @@ trait RestController
 
         if ($result instanceof Collection) {
             $result = $result->all();
+        } elseif ($result instanceof Resourcable) {
+            $result = $result->asResource();
         }
 
         if (is_array($result)) {
