46a8dbe6698195565a70c449ca1d25ebe390d341
Alex Westergaard <aaw@subit.dk>
2022-09-12 16:30:12 +0200

    Add dockblock to Parser Interface

--- src/Parsing/Parser.php
+++ src/Parsing/Parser.php
@@ -1,15 +1,35 @@
 <?php
 
-
 namespace Seier\Resting\Parsing;
 
-
+/**
+ * @method mixed shouldParse(ParseContext $context)
+ * @method mixed canParse(ParseContext $context)
+ * @method mixed parse(ParseContext $context)
+ */
 interface Parser
 {
-
+    /**
+     * Determine wether or not a value should be parsed
+     *
+     * @param ParseContext $context
+     * @return boolean
+     */
     public function shouldParse(ParseContext $context): bool;
 
+    /**
+     * Determine wether or not a value fits this parser
+     *
+     * @param ParseContext $context
+     * @return array
+     */
     public function canParse(ParseContext $context): array;
 
+    /**
+     * Parse the value
+     *
+     * @param ParseContext $context
+     * @return mixed
+     */
     public function parse(ParseContext $context): mixed;
 }
